7f15a998e728a0c445ecbaec90456a5f411d0915
Altair Inglorious <>
2022-11-21 17:10:47 +0200

    new department fetch

--- components/Department/DepartmentList.jsx
+++ components/Department/DepartmentList.jsx
@@ -1,29 +1,24 @@
 import DepartmentCard from "./DepartmentCard"
 
 
-export default function DepartmentList(props){
-	const projectList = [
-		{
-			id: 1,
-			name: 'Department 1'
-		},
-		{
-			id: 2,
-			name: 'Department 2'
-		},
-		{
-			id: 3,
-			name: 'Department 3'
-		},
-		{
-			id: 4,
-			name: 'Department 4'
-		},
-	]
+async function getDepartmentList(id){
+	let departmentList = await fetch('http://185.181.8.111:1337/api/departments?filters[project][id][$eq]='+id,
+	{
+		headers: {
+			'Authorization': 'Bearer d47807a3d353011a939c7e00374285ca07cf58b1c208b27c3ae6b678cc83fd6a5fbf250dcad2146d28d0968a8507157eb6bb46f65c48cbab9566b33ddf9ffc7e23307d7efbaa72e9ac3ae240f969d3e707fb139105eda6682a59eb477a581b5df453360535f4ab8991abf1472c62407f1eb7f02df5332e1a782179ff9bf37a64'
+		}
+	})
+
+	return departmentList.json()
+}
+
+
+export default async function DepartmentList(props){
+	const departmentList = await getDepartmentList(props.id)
 	return (
 		<>
 		<h1>Project: {props.id}</h1>
-		{projectList.map((el, index) => <DepartmentCard key={index} {...el}/>)}
+		{departmentList.data.map((el) => <DepartmentCard key={el.id} id={el.id} {...el.attributes}/>)}
 		</>
 	)
 }

56dd90b6b7b592508e572da2e4fbcc9a160bf8f1
AltairInglorious <113914468+AltairInglorious@users.noreply.github.com>
2022-11-24 14:21:23 +0000

    fix

--- components/Department/DepartmentList.jsx
+++ components/Department/DepartmentList.jsx
@@ -1,28 +1,41 @@
+'use client'
 import DepartmentCard from "./DepartmentCard"
 import AddDepartment from './AddDepartment'
-
-
-async function getDepartmentList(id){
-	let departmentList = await fetch('https://altairdev.tech/api/departments?filters[project][id][$eq]='+id,
-	{
-		headers: {
-			'Authorization': 'Bearer d47807a3d353011a939c7e00374285ca07cf58b1c208b27c3ae6b678cc83fd6a5fbf250dcad2146d28d0968a8507157eb6bb46f65c48cbab9566b33ddf9ffc7e23307d7efbaa72e9ac3ae240f969d3e707fb139105eda6682a59eb477a581b5df453360535f4ab8991abf1472c62407f1eb7f02df5332e1a782179ff9bf37a64'
-		}
-	})
-
-	return departmentList.json()
-}
+import { useEffect, useState } from 'react'
 
 
 export default async function DepartmentList(props){
-	const departmentList = await getDepartmentList(props.id)
+	const [departmentList, setDepartmentList] = useState(() => '')
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			let fetchedData = await fetch('https://altairdev.tech/api/departments?filters[project][id][$eq]='+id,
+			{
+				headers: {
+					'Authorization': 'Bearer '+localStorage.getItem('token')
+				}
+			})
+			.then((res) => res.json())
+			.then((data) => {return data})
+			
+			setDepartmentList(fetchedData)
+			setLoading(false)
+		}
+		fetchData()
+	}, [])
+
 	return (
-		<div className='flex flex-col justify-around items-center'>
-			<h1 className='font-bold'>Project: {props.id}</h1>
-			<div className='flex flex-wrap justify-around items-center w-full'>
-				<AddDepartment id={props.id}/>
-				{departmentList.data.map((el) => <DepartmentCard key={el.id} id={el.id} {...el.attributes}/>)}
+		loading
+		? <h1>⏳ Loading...</h1>
+		: (
+			<div className='flex flex-col justify-around items-center'>
+				<h1 className='font-bold'>Project: {props.id}</h1>
+				<div className='flex flex-wrap justify-around items-center w-full'>
+					<AddDepartment id={props.id}/>
+					{departmentList.data.map((el) => <DepartmentCard key={el.id} id={el.id} {...el.attributes}/>)}
+				</div>
 			</div>
-		</div>
+		)
 	)
 }
--- components/Task/TaskList.jsx
+++ components/Task/TaskList.jsx
@@ -1,25 +1,38 @@
+'use client'
 import TaskCard from "./TaskCard"
 import AddTask from './AddTask'
-
-
-async function getDepartmentList(id){
-	let departmentList = await fetch('https://altairdev.tech/api/tasks?sort[0]=compleate&sort[1]=end_date&filters[department][id][$eq]='+id,
-	{
-		headers: {
-			'Authorization': 'Bearer d47807a3d353011a939c7e00374285ca07cf58b1c208b27c3ae6b678cc83fd6a5fbf250dcad2146d28d0968a8507157eb6bb46f65c48cbab9566b33ddf9ffc7e23307d7efbaa72e9ac3ae240f969d3e707fb139105eda6682a59eb477a581b5df453360535f4ab8991abf1472c62407f1eb7f02df5332e1a782179ff9bf37a64'
-		}
-	})
-	console.log(departmentList)
-	return departmentList.json()
-}
+import { useState, useEffect } from 'react'
 
 
 export default async function TaskList(props){
-	const taskList = await getDepartmentList(props.id)
+	const [taskList, setTaskList] = useState()
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			let fetchedData = await fetch('https://altairdev.tech/api/tasks?sort[0]=compleate&sort[1]=end_date&filters[department][id][$eq]='+id,
+			{
+				headers: {
+					'Authorization': 'Bearer '+localStorage.getItem('token')
+				}
+			})
+			.then((res) => res.json())
+			.then((data) => {return data})
+
+			setTaskList(fetchedData)
+			setLoading(false)
+		}
+		fetchData()
+	}, [])
+
 	return (
-		<div  className='flex flex-wrap justify-around items-center'>
-		<AddTask id={props.id}/>
-		{taskList.data.map((el) => <TaskCard key={el.id} id={el.id} {...el.attributes}/>)}
-		</div>
+		loading
+		? <h1>⏳ Loading...</h1>
+		: (
+			<div  className='flex flex-wrap justify-around items-center'>
+			<AddTask id={props.id}/>
+			{taskList.data.map((el) => <TaskCard key={el.id} id={el.id} {...el.attributes}/>)}
+			</div>
+		)
 	)
 }
